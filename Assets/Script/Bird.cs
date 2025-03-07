d79cd5d3f3d03410d52aa7a67091b7a0756c7d7d
lyona7x <lyona12344@gmail.com>
2025-03-07 23:33:55 +0300

    first commit

---
+++ Assets/Script/Bird.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    private bool ispressed= false;
+    public Rigidbody2D rb;
+    public Rigidbody2D DragPoint;
+    public SpringJoint2D springJoint2D;
+    private float dragRelaseTime = 0.15f;
+    private float maxDragDistance = 2f;
+    private Transform playerTransform;
+    bool isJumped;
+    void Start()
+    {
+         playerTransform = gameObject.transform;
+         isJumped = false;
+         Debug.Log(playerTransform.position.x);
+    }
+
+    void Update()
+    {
+        if (ispressed)
+        {
+            Vector2 mousPos= Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (Vector3.Distance(mousPos,DragPoint.position)> maxDragDistance)
+            {
+                rb.position = DragPoint.position + (mousPos -DragPoint.position).normalized *maxDragDistance;
+            }
+            else
+            {
+                rb.position= mousPos;
+            }
+        }
+
+        if(transform.position.x>10)
+        {
+            gameObject.GetComponent<Rigidbody2D>().simulated = false;
+            transform.position = new Vector2(-6,0.5f);
+            springJoint2D.enabled = true;
+             gameObject.GetComponent<Rigidbody2D>().simulated = true;
+        }
+
+    }
+
+    void OnMouseDown()
+    {
+        ispressed = true;
+        rb.isKinematic = true;
+    }
+
+    void OnMouseUp()
+    {
+        ispressed = false;
+        rb.isKinematic = false;
+        StartCoroutine(RelaseDrag());
+        isJumped = false;
+    }
+
+    IEnumerator RelaseDrag ()
+    {
+        yield return new WaitForSeconds(dragRelaseTime);
+        springJoint2D.enabled = false;
+        
+    }
+}
---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private ParticleSystem particlesys;
+    public float heatlh= 4f;
+
+    public static int NumberofEnemies=0;
+
+    void Start()
+    {
+        particlesys = GetComponentInChildren<ParticleSystem>();
+        NumberofEnemies++;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.relativeVelocity.magnitude>heatlh)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        particlesys.Play();
+        Destroy(gameObject,0.1f);  
+        NumberofEnemies--;
+        if (NumberofEnemies<=0)
+        {
+            
+        }  
+    }
+}
